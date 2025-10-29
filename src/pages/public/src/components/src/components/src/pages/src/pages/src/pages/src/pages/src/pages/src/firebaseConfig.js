8647b029eb6b79b418a48f783c4b777935282e49
rhodiumabillions14-lgtm <rhodium.a.billions14@gmail.com>
2025-10-29 23:07:35 +0000

    Add Firebase Auth initialization to firebaseConfig.js

--- src/pages/public/src/components/src/components/src/pages/src/pages/src/pages/src/pages/src/pages/src/firebaseConfig.js
+++ src/pages/public/src/components/src/components/src/pages/src/pages/src/pages/src/pages/src/pages/src/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
+import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA2LK2DomN_aRjf0cpV8I7UsFU1OIx3LpQ",
@@ -16,5 +16,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export { app, analytics };
+const auth = getAuth(app);
+export { app, analytics, auth };

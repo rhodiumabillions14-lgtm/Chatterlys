7f6095d1c1481383cffce077159bb28cb7f95a0b
rhodiumabillions14-lgtm <rhodium.a.billions14@gmail.com>
2025-10-29 21:37:14 +0000

    Initialize App component with routing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}

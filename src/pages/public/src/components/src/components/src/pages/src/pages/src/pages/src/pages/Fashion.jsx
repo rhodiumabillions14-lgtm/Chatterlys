1961fdeea9d8a2abf5f19b3324b0cb5d0db3cd11
rhodiumabillions14-lgtm <rhodium.a.billions14@gmail.com>
2025-10-29 22:17:29 +0000

    Add Fashion component with sample posts

---
+++ src/pages/public/src/components/src/components/src/pages/src/pages/src/pages/src/pages/Fashion.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Fashion() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Chatterly - Fashion</h2>
+      <div style={{ marginBottom: "20px" }}>
+        <strong>Bob:</strong> Check out my new design concept!<br />
+        <em>2 likes · 0 comments</em>
+      </div>
+      <div>
+        <strong>Charlie:</strong> What do you think of this color palette?<br />
+        <em>0 likes · 1 comment</em>
+      </div>
+    </div>
+  );
+}
